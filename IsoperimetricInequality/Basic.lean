3791d3284999543a77d2458774b799b39afc187d
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-02-15 08:46:15 -0600

    defined fourier series

--- IsoperimetricInequality/Basic.lean
+++ IsoperimetricInequality/Basic.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import Mathlib.Analysis.Fourier.FourierTransform
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
+import Mathlib.Topology.Algebra.InfiniteSum.Basic
+
+open Real
+open scoped BigOperators Topology
+
+noncomputable section
+
+-- The Fourier coefficients
+variable (a : ℕ → ℝ) (b : ℕ → ℝ)
+
+-- The classical Fourier series as a function of x
+def fourierSeries (x : ℝ) : ℝ :=
+    (1/2) * a 0 + ∑' n : ℕ+, (a n * cos (n * x) + b n * sin (n * x))
+
+
+end
