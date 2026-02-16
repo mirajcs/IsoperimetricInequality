cd39604690c3e1351655c084b16ea713e06a777e
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-02-16 07:22:50 -0600

    started a new theorem about squre of fourier

--- IsoperimetricInequality/Basic.lean
+++ IsoperimetricInequality/Basic.lean
@@ -14,5 +14,17 @@ variable (a : ℕ → ℝ) (b : ℕ → ℝ)
 def fourierSeries (x : ℝ) : ℝ :=
     (1/2) * a 0 + ∑' n : ℕ+, (a n * cos (n * x) + b n * sin (n * x))
 
+-- The sum part of fourier series
+def fourierSum (x : ℝ) : ℝ :=
+    ∑' n : ℕ+, (a n * cos (n * x) + b n * sin (n * x))
+
+-- f(x) = a₀/2 + S(x) where is S is the sum
+lemma fourierSeries_eq (x : ℝ) :
+  fourierSeries a b x = (1/2) * a 0 + fourierSum a b x := by rfl 
+
+-- The expansion of [f(x)]² 
+theorem fourierSeries_sq (x : ℝ) : by 
+  sorry 
+
 
 end
